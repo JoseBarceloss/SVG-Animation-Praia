d8f07cd865f646802bc9b4d459c71938b8f92849
Barcelos <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-06-13 06:59:25 -0300

    Terminei as animações do cards

---
+++ src/script/card.js
@@ -0,0 +1,31 @@
+class Cards {
+
+//construtor que pega os elementos do html
+constructor() {
+    this.posicaoSectionDicas = document.getElementById('section-dicas')
+    this.cardEsq = document.querySelector('.card-esq')
+    this.cardDir = document.querySelector('.card-dir')
+}   
+
+//função que chama o calculo do scroll
+scrollcards () {
+    window.requestAnimationFrame(this.calculoScroll.bind(this));
+}
+
+
+//calcula o scroll da pagina e aplica a transição nos cards
+calculoScroll() {
+    const posicao = this.posicaoSectionDicas.getBoundingClientRect()['y']
+
+    console.log(posicao)
+
+    if (posicao >= 25) {
+        this.cardEsq.style.transform = `translate(${(-posicao + 25) /7}%)`;
+        this.cardDir.style.transform = `translate(${(posicao - 25) /7}%)`;
+    }
+}
+}
+
+
+
+export { Cards }    
---
+++ src/script/index.js
@@ -0,0 +1,8 @@
+import { Cards } from './card.js';
+
+// Função que chama a animação dos cards
+window.onload = () => {
+    let animationCard = new Cards();
+    document.addEventListener('scroll', animationCard.
+        scrollcards.bind(animationCard));
+};
